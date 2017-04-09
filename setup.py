b9eb660d8a09e5a301791775f92867ba54a03587
Andrei Lapets <a@lapets.io>
2017-04-08 23:55:21 -0400

    Adjust setup and release scripts.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version          = '0.0.6.0',
     packages         = ['geoql',],
     install_requires = ['geojson', 'geopy', 'rtree', 'shapely', 'tqdm',],
-    license          = 'MIT License',
+    license          = 'MIT',
 	url              = 'https://github.com/Data-Mechanics/geoql',
 	author           = 'Andrei Lapets',
 	author_email     = 'a@lapets.io',
     description      = 'Library for performing queries and transformations on GeoJSON data (with emphasis on support for abstract graph representations).',
-    long_description = open('README').read(),
+    long_description = open('README.rst').read(),
 )
